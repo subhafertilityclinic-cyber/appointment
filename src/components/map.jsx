a30c75a903f03753ba4fc7c4de39e39beb14b64f
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-27 00:32:03 +0545

    mobile first approach

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -10,8 +10,8 @@ const Map = React.memo(() => {
     <MapContainer
       center={[27.679466469087807, 84.4318247396386]}
       zoom={17}
-      scrollWheelZoom={true}
-      className="w-full max-w-[360px] min-w-[300px] h-[200px] border-2 border-main-gray rounded-2xl "
+      scrollWheelZoom={false}
+      className="w-full max-w-maxi min-w-[300px] h-[200px] border-2 border-main-gray rounded-2xl "
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,12 @@
 const Nav = () => {
-  return <nav className="border-y-2 border-main-gray py-7 flex flex-col w-full max-w-[360px] min-w-[300px] " >
+  return <nav className="border-y-2 border-main-gray pl-2  py-7 flex flex-col w-full max-w-maxi min-w-[300px] " >
     <img src="/doc.jpeg" className="w-30 h-30 rounded-full border-2 border-main-green " alt="fertility expert" />
     <p className="font-heading text-lg text-main-black tracking-wide " >Fertility Expert</p>
     <h2 className="font-heading font-bold text-main-black text-3xl tracking-wider " >Dr.Subha Shrestha</h2>
     <ul className="font-heading text-lg " >
       <li>MBBS NMC no:6395</li>
       <li>MD Obstetrics &amp; Gynecology</li>
-      <li>AET &amp; IVF Fellowship</li>
+      <li>ART &amp; IVF Fellowship</li>
     </ul>
   </nav>
 }
