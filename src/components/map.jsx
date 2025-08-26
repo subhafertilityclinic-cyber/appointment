835082ac7279993fefdc705232e27042b2e75213
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-27 00:53:01 +0545

    location svg

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,7 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from "leaflet"
 import 'leaflet/dist/leaflet.css';
 import React from 'react';
 
+const svgMarker = L.icon({
+  iconUrl: "/location.svg",
+  iconSize: [38, 38],      // adjust to your SVG size
+  iconAnchor: [16, 32],    // bottom-center of icon
+  popupAnchor: [0, -32],
+});
 const Map = React.memo(() => {
   return (<section className="w-full max-w-[360px] min-w-[300px] flex flex-col items-center mt-2 " >
     <strong className="font-bold font-heading text-main-black text-2xl  " >
@@ -17,7 +24,7 @@ const Map = React.memo(() => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[27.679466469087807, 84.4318247396386]}>
+      <Marker position={[27.679466469087807, 84.4318247396386]} icon={svgMarker} >
         <Popup className="font-bold text-main-black font-heading" >
           Subha Fertility <br /> clinic
         </Popup>
