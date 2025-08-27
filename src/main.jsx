91811f594b5c8d19fd9168ce1512a1ce1969c8de
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-27 19:31:23 +0545

    about added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import About from './About.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+  </BrowserRouter>
+);
