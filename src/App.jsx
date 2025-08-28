92ff7e4265149b4aec284e05ecac28eb7493b14f
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-28 12:05:02 +0545

    Fixed about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header.jsx"
 import Form from "./components/form.jsx"
 import Map from "./components/map.jsx"
 import Footer from "./components/footer.jsx"
+import Experts from "./components/experts.jsx"
 
 const App = () => {
   return <>
@@ -9,6 +10,7 @@ const App = () => {
     <main className="flex items-center flex-col " >
       <Form />
       <Map />
+      <Experts />
     </main>
     <Footer />
   </>
