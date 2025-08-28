76e49fc23aaee3bb2aa5a021a210efe4e595ec99
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-28 22:21:31 +0545

    changed theme to pink

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -18,7 +18,7 @@ const Map = React.memo(() => {
       center={[27.679466469087807, 84.4318247396386]}
       zoom={17}
       scrollWheelZoom={false}
-      className="w-full max-w-maxi min-w-[300px] h-[200px] border-2 border-main-gray rounded-2xl "
+      className="w-full max-w-maxi min-w-[300px] h-[200px] border-2 border-main-accent rounded-2xl "
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,8 +1,8 @@
 const Nav = () => {
-  return <nav className="border-y-2 border-main-gray pl-2  py-7 flex flex-col w-full max-w-maxi min-w-[300px] " >
-    <img src="/doc.jpeg" className="w-30 h-30 rounded-full border-2 border-main-green " alt="fertility expert" />
+  return <nav className="border-y-2 border-main-accent pl-2  py-7 flex flex-col w-full max-w-maxi min-w-[300px] " >
+    <img src="/doc.jpeg" className="w-30 h-30 rounded-full border-2 border-main-accent " alt="fertility expert" />
     <p className="font-heading text-lg text-main-black tracking-wide " >Fertility Expert</p>
-    <h2 className="font-heading font-bold text-main-black text-3xl tracking-wider " >Dr.Subha Shrestha</h2>
+    <h2 className="font-heading font-bold text-main-accent text-3xl tracking-wider " >Dr.Subha Shrestha</h2>
     <ul className="font-heading text-main-black text-lg " >
       <li>MBBS NMC no:6395</li>
       <li>MD Obstetrics &amp; Gynecology</li>
