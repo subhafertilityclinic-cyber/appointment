d4664aa0f8d9132dcce86b7a9424a5f69ade3b47
sumitpoudel <sumitpoudel325@gmail.com>
2025-08-31 01:59:12 +0545

    map fixes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
-const Header = ({children}) => {
-  return <header className="pt-2" >
+const Header = ({ children }) => {
+  return <header className="" >
     <div className="flex flex-col justify-center items-center " >
-      <div className="flex bg-main-background w-full md:pl-10 items-center " >
-        <img src="/logo.png" className="w-15 h-15 md:h-20 md:w-20 " alt="logo" />
-        <h1 className="font-logo font-bold text-3xl text-main-primary md:text-5xl " >Subha fertility clinic</h1>
+      <div className="flex bg-linear-to-bl py-1 bg-blend-soft-light from-purple-100/60 via-fuchsia-50 to-pink-100/60 w-full md:pl-10 items-center " >
+        <img src="/logo.png" className="w-15 h-15" alt="logo" />
+        <h1 className="font-logo font-semibold text-2xl text-fuchsia-800 " >subha fertility clinic</h1>
       </div>
-    {children ? children : null}
+      {children ? children : null}
     </div>
   </header>
 }
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -36,32 +36,49 @@ const getLocation = (() => {
 
 const Map = React.memo(() => {
   return (<section className=" w-full flex justify-center px-6 " >
-    <div className='bg-white h-96 w-full mt-10 md:w-[70vw] rounded-2xl px-4 md:px-10  py-4 flex flex-col items-center shadow-md'>
+    <div className='bg-white  w-full md:flex-row mt-10 md:w-[70vw] rounded-2xl px-4 md:px-10  py-4 flex flex-col items-center shadow-md'>
       <strong className="font-bold font-heading text-main-black text-2xl md:text-3xl mb-5 " >
         Visit Us
       </strong>
-      <MapContainer
-        center={[27.679466469087807, 84.4318247396386]}
-        zoom={17}
-        scrollWheelZoom={false}
-        className=" w-full h-100  border-2 border-main-accent  rounded-2xl mb-4"
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[27.679466469087807, 84.4318247396386]} icon={svgMarker} >
-          <Popup className="font-bold text-main-black font-heading"   >
-            <p> Subha Fertility clinic</p>
-            <button onClick={getLocation} className='text-blue-600 p-3'>Get Directions</button>
-          </Popup>
-        </Marker>
-      </MapContainer>
-      <button onClick={getLocation} className="px-4 h-17 md:font-bold md:text-lg w-full 
+      <div className='w-full flex justify-center items-center flex-col ' >
+        <MapContainer
+          center={[27.679466469087807, 84.4318247396386]}
+          zoom={17}
+          scrollWheelZoom={false}
+          className=" w-full h-[40vh] md:h-[50vh] md:w-[50vw]   border-2 border-main-accent  rounded-2xl m-4"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[27.679466469087807, 84.4318247396386]} icon={svgMarker} >
+            <Popup className="font-bold text-main-black font-heading"   >
+              <p> Subha Fertility clinic</p>
+              <button onClick={getLocation} className='text-blue-600 p-3'>Get Directions</button>
+            </Popup>
+          </Marker>
+        </MapContainer>
+        <button
+          onClick={() => { window.open("https://maps.app.goo.gl/gEFzXum7TwW3V7js8", "_blank") }}
+          className="h-10 block lg:hidden mt-3 w-full max-w-maxi md:font-bold md:text-lg 
              rounded-2xl bg-main-accent text-main-background font-semibold 
              active:brightness-90 
              hover:brightness-105 hover:shadow-md hover:cursor-pointer 
-             transition duration-200 ease-in-out">Get location</button>
+             transition duration-200 ease-in-out"
+        >
+          get Location
+        </button>
+        <button
+          onClick={getLocation}
+          className="h-10 hidden lg:block mt-3 w-full max-w-maxi md:font-bold md:text-lg 
+             rounded-2xl bg-main-accent text-main-background font-semibold 
+             active:brightness-90 
+             hover:brightness-105 hover:shadow-md hover:cursor-pointer 
+             transition duration-200 ease-in-out"
+        >
+          get Location
+        </button>
+      </div>
     </div>
   </section>
   );
