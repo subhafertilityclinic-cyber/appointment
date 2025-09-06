321950f1373682833d9077348c46bfa36644d46a
sumitpoudel <sumitpoudel325@gmail.com>
2025-09-06 18:24:11 +0545

    sitemap
    site map added for seo and few css fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
