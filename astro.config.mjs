67c821939c20a9d960d6c27af717f40997cd143d
sumit <sumitpoudel325@gmail.com>
2025-09-23 21:26:44 +0545

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "never",
+  site: "https://subhafertility.org.np",
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react(), sitemap()],
 });
